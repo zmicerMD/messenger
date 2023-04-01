0585a27cd1f67bcec6f9a9b5bb3e6692c0184f91
MDz1985 <m_d@mail.uk>
2023-04-01 17:05:11 +0200

    feat: add contacts-card-component

---
+++ src/standalone/contact-card/contact-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contact-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact-card.component.html',
+  styleUrls: ['./contact-card.component.scss']
+})
+export class ContactCardComponent {
+
+}
