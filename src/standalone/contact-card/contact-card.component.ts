dcf72b4b18310d0177d4bcf712f93db7b8e8e950
MDz1985 <m_d@mail.uk>
2023-04-17 10:15:30 +0200

    feat: add basic card layout

--- src/standalone/contact-card/contact-card.component.ts
+++ src/standalone/contact-card/contact-card.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+interface ICardsData {
+  name: string,
+  icon: string,
+  lastSeen: Date,
+  lastMessages: string[],
+}
 
 @Component({
   selector: 'app-contact-card',
@@ -9,5 +16,8 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./contact-card.component.scss']
 })
 export class ContactCardComponent {
+  @Input() cardsData?: ICardsData;
 
+  avatar?: string = this.cardsData?.icon;
+  avatarAlt? = this.cardsData?.name.slice(0, 1);
 }
